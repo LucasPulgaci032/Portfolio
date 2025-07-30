bb3ca6cf41bf45c7df4b7b0b2c57f82381e0cd03
LucasPulgaci032 <163359576+LucasPulgaci032@users.noreply.github.com>
2025-07-29 21:03:40 -0300

    Update StylesForHello.js

--- portfolio-lucas/src/Styles/StylesForHello.js
+++ portfolio-lucas/src/Styles/StylesForHello.js
@@ -13,25 +13,28 @@ export const EntranceWords = keyframes`
     opacity: 1;
   }
 `;
-
 export const Inicialization = styled.div`
   display: flex;
   flex-direction: column;
   gap: 1rem;
-  font-size: clamp(1.2rem, 2.5vw, 2rem);
   width: 100%;
   max-width: 600px;
   margin-top: 15vh;
-  font-weight: bold;
-  text-shadow: 0 0 0.1px #9b30ff, 0 0 1px #8a2be2, 0 0 15px #7b68ee;
-  animation: ${EntranceWords} 0.8s cubic-bezier(0.215, 0.610, 0.355, 1.000) both;
   padding-left: 1.5rem;
   padding-right: 1.5rem;
   transition: all 0.3s ease-in-out;
+`;
+
+export const Title = styled.h1`
+  font-weight: bold;
+  text-shadow: 0 0 0.1px #9b30ff, 0 0 1px #8a2be2, 0 0 15px #7b68ee;
+  animation: ${EntranceWords} 0.8s cubic-bezier(0.215, 0.610, 0.355, 1.000) both;
+
+  font-size: clamp(1.2rem, 2.5vw, 2rem);
 
   @media (max-width: 768px) {
     font-size: clamp(1rem, 4vw, 1.5rem);
-    padding-left: 1rem;
-    padding-right: 1rem;
   }
+`;
+
 `;
